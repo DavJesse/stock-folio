dadfa93a5ce726add8b85558fbc1c1a64886eff0
david-jesse-odhiambo <david.odhiambo@01talent.ke>
2025-07-15 17:42:41 +0300

    chore: add interim page.tsx for UI previewing. (#9.3)

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import SignUpForm from '@/components/SignUpForm'
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-8">
+      <SignUpForm />
+    </div>
+  )
+}
