caf0dee6aac8abe6ba53050a36201154fa04e79d
david-jesse-odhiambo <david.odhiambo@01talent.ke>
2025-07-15 17:40:52 +0300

    chore: add layout.tsx to define html document (#9.3)

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+// Import global CSS styles — applies to the entire app
+import './globals.css'
+
+/**
+ * Metadata used by Next.js for SEO and document head generation.
+ * This object is used internally by the framework (e.g. <Head /> injection).
+ */
+export const metadata = {
+  title: 'stock-portfolio-tracker',
+  description: 'Lantel Github assessment by David Jesse Odhiambo',
+}
+
+/**
+ * Root layout component that wraps all pages.
+ * Placed in `app/layout.tsx` to define global structure like <html> and <body>.
+ */
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
