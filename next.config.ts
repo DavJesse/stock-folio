10c576f22a66a5b89ba985ea1d6cf61608f81869
david-jesse-odhiambo <david.odhiambo@01talent.ke>
2025-07-19 10:06:31 +0300

    chore: add headers to aid in CSRF (#10.5)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+const isTest = process.env.NODE_ENV === 'test'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: isTest ? [] : [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self'; object-src 'none';" },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'same-origin' },
+        ],
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
