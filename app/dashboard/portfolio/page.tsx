aaa11a230506ae6c757e0fd344d8745291a3d203
david-jesse-odhiambo <david.odhiambo@01talent.ke>
2025-08-10 17:28:01 +0300

    hotfix: update vertical spacing (#82.3)

--- app/dashboard/portfolio/page.tsx
+++ app/dashboard/portfolio/page.tsx
@@ -6,9 +6,8 @@ import StockSearchBar from '@/components/SearchBar'
 // Renders content of /dashboard/portfolio
 export default function PortfolioPage() {
   return (
-    <div>
+    <div className="space-y-8">
       <StockSearchBar /> {/* Render search bar at the top */}
-      <h1 className="text-2xl text-white font-bold mt-10">Portfolio Overview</h1>
       <PortfolioOverview />
     </div>
   )
